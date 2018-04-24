5d4663031a5a8681c7d1e39334f3cb1592d2462e
Alice <a.j.stack@sbcglobal.net>
2018-04-24 17:49:50 -0500

    starting javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+var randomNumber = $("#random-number");  // Number needed to be matched
+var win = $("win");                      // How many times the user has won
+var lose = $("lose");                    // How many times the user has lost
+var gameAlert = $("game-alert");         // Display if won or lost
