85c9b39da21e8f0f617a63c01f8f0a4f537ef4d7
Alice <a.j.stack@sbcglobal.net>
2018-04-24 18:32:07 -0500

    adding javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,34 @@
+var jewels = [
+    "opal",
+    "ruby",
+    "sapphire",
+    "steven"
+];
 var randomNumber = $("#random-number");  // Number needed to be matched
 var win = $("win");                      // How many times the user has won
 var lose = $("lose");                    // How many times the user has lost
 var gameAlert = $("game-alert");         // Display if won or lost
+var score = $("score");                  // Display current user number
+var winCount = 0;                        // Initial win count
+var lossCount = 0;                       // Initial loss count
+ 
+$(document).ready(function(){
+
+    // Display how many times user has won or lost {not working}
+    $(win).text(winCount);
+    $(lose).text(lossCount);
+    
+    // Assign values to jewels {I think this is working}
+    for (i = 0; i < jewels.length; i++){
+        jewels[i] = Math.floor((Math.random() * 11) + 1)
+    }
+        console.log(jewels);
+
+
+
+
+
+
+
+
+});
