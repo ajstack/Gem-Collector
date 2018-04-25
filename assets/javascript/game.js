de17857554cc8ce4f6f4ceaa4c0ba930670bef92
Alice <a.j.stack@sbcglobal.net>
2018-04-24 19:35:30 -0500

    pseudocoding javascript minor css changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,31 +4,54 @@ var jewels = [
     "sapphire",
     "steven"
 ];
-var randomNumber = $("#random-number");  // Number needed to be matched
-var win = $("win");                      // How many times the user has won
-var lose = $("lose");                    // How many times the user has lost
+//var randomNumber = $("#random-number");  // Number needed to be matched
 var gameAlert = $("game-alert");         // Display if won or lost
-var score = $("score");                  // Display current user number
+var score = 0;                  // Display current user number
 var winCount = 0;                        // Initial win count
 var lossCount = 0;                       // Initial loss count
  
 $(document).ready(function(){
 
-    // Display how many times user has won or lost {not working}
-    $(win).text(winCount);
-    $(lose).text(lossCount);
+    // Display how many times user has won or lost {I got this to work by getting rid of the variables and linking directly to the html}
+    $("#win").text(winCount);
+    $("#lose").text(lossCount);
+    $("#score").text(score);
     
     // Assign values to jewels {I think this is working}
     for (i = 0; i < jewels.length; i++){
-        jewels[i] = Math.floor((Math.random() * 11) + 1)
+        jewels[i] = Math.floor((Math.random() * 11) + 1);
     }
-        console.log(jewels);
+        //console.log(jewels);
+      
+    // Pick random number to match
+    randomNumber = Math.floor((Math.random() * 101) + 19);
+    //console.log(randomNumber);
 
+    // Display random number 
+    $("#random-number").text(randomNumber);
 
+    function reset(){
+        for (i = 0; i < jewels.length; i++){
+            jewels[i] = Math.floor((Math.random() * 11) + 1);
+        }
+        randomNumber = Math.floor((Math.random() * 101) + 19);
+        $("#random-number").text(randomNumber);
+    };
 
+    function winner(){
+        //Display "you win!" to #game-alert
+        //winCount++
+        //reset()
+    };
 
+    function loser(){
+        //Display "you lose!" to #game-alert
+        //lossCount++
+        //reset()
+    };
 
+    $("opal").click(function(){
 
-
+    });
 
 });
